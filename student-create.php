4e0556617c1e98557b20ed0d05e23b4958f4ddab
2010101289 <danielodf2.aluno@unipampa.edu.br>
2022-08-16 17:12:47 -0300

    iniciando criação crud-php

---
+++ student-create.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Student Add</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Student Add
+                            <a href="index.php" class="btn btn-danger float-end">back</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="code.php" method="POST">
+                            <div class="mb-3">
+                                <label>Student Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Student Email</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Student Phone</label>
+                                <input type="text" name="phone" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Student Course</label>
+                                <input type="text" name="couse" class="form-control">
+                            </div> 
+                            <div class="mb-3">
+                                <button type="submit" name='save-student' class="btn btn-primary">Save Student</button>
+                            </div>   
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
